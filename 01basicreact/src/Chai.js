641c1ca6ef59b8924054a1116b2028a8f3007df6
Rohan Pokwal <rohan.pokwal@gmail.com>
2024-08-12 19:07:33 -0500

    Second Commit

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,11 @@
+
+
+function Chai(){
+    return(
+        <h2>
+            Chain in React
+        </h2>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+  
+
+  return (
+    <>
+
+    <Chai/>
+    <h1>Chai</h1>
+    </>
+    
+  )
+}
+
+export default App
